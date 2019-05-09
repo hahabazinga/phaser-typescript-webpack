9a5e32bafbdb4a8bbdb774d8cdcc441f6a95edbb
wangshuai <wangshuai@momenta.ai>
2019-05-09 14:24:52 +0800

    [init] first commit

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import * as Phaser from "phaser";
+import * as logoImg from "./assets/logo.png";
+
+const config: GameConfig = {
+  type: Phaser.AUTO,
+  parent: "phaser-example",
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("logo", logoImg);
+}
+
+function create() {
+  const logo = this.add.image(400, 150, "logo");
+
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: "Power2",
+    yoyo: true,
+    loop: -1
+  });
+}
